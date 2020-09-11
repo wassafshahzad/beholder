6a80b33da9475895484becfa0cfedcabfd1fa6de
wassaf <wassafshahzad@gmail.com>
2020-09-11 09:35:36 +0500

    added setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='beholder',
+      version='1.0',
+      description='A python module for type checking function parameters and returns',
+      url='https://github.com/wassafshahzad/beholder',
+      author='Wassaf Shahzad',
+      author_email='wassafshahzad@gmail.com',
+      license='MIT',
+      packages=['beholder'],
+      zip_safe=False)
