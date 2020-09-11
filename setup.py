fdad4163a8590a62572d02e4a7264243b4abd4ae
Wassaf Shahzad <wassaf.shahzad@fiveriverstech.com>
2020-09-11 05:55:41 -0400

    renamed folder

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
-setup(name='beholder',
-      version='1.0',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='be_holder',
+      version='1.1',
       description='A python module for type checking function parameters and returns',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/wassafshahzad/beholder',
       author='Wassaf Shahzad',
       author_email='wassafshahzad@gmail.com',
